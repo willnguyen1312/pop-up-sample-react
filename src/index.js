ed3f9db0da4c561eac1591df630f351d31714447
willnguyen1312 <willnguyen131292@outlook.com>
2018-03-29 23:17:11 +0700

    Remove unused style

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
-};
-
 render(<App />, document.getElementById("root"));
